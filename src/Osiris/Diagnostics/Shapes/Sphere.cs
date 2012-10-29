5e31365b0df698d025f9e17abab690bd198dfa2b
Tim Jones <tim@timjones.tw>
2012-10-29 22:34:14 +0800

    Better sphere visualization

--- src/Osiris/Diagnostics/Shapes/Sphere.cs
+++ src/Osiris/Diagnostics/Shapes/Sphere.cs
@@ -8,14 +8,27 @@ namespace Osiris.Diagnostics.Shapes
 			Vector3 cameraPosition, Matrix cameraView, Matrix cameraProjection,
 			Vector3 center, float radius, Quaternion rotation, Color color)
 		{
-			// Three discs
-			Disc.DrawWireframe(primitiveDrawer, cameraPosition, cameraView, cameraProjection, center, Vector3.Transform(Vector3.Up, rotation), radius, color, true);
-			Disc.DrawWireframe(primitiveDrawer, cameraPosition, cameraView, cameraProjection, center, Vector3.Transform(Vector3.Forward, rotation), radius, color, true);
-			Disc.DrawWireframe(primitiveDrawer, cameraPosition, cameraView, cameraProjection, center, Vector3.Transform(Vector3.Left, rotation), radius, color, true);
+			// Draw three orthogonal discs.
+			Disc.DrawWireframe(primitiveDrawer, cameraPosition, cameraView, cameraProjection, center, Vector3.Normalize(Vector3.Transform(Vector3.Up, rotation)), radius, color, true);
+			Disc.DrawWireframe(primitiveDrawer, cameraPosition, cameraView, cameraProjection, center, Vector3.Normalize(Vector3.Transform(Vector3.Forward, rotation)), radius, color, true);
+			Disc.DrawWireframe(primitiveDrawer, cameraPosition, cameraView, cameraProjection, center, Vector3.Normalize(Vector3.Transform(Vector3.Left, rotation)), radius, color, true);
+
+			// Draw disc aligned with camera. To do this, first calculate the largest visible cross section using
+ 			// the technique described here: http://www.quantimegroup.com/solutions/pages/Article/Article.html
+
+			// Solve for dy.
+			Vector3 cameraToCenter = center - cameraPosition;
+			float distanceToCenter = cameraToCenter.Length();
+			float radius2 = radius * radius;
+			float dy = radius2 / distanceToCenter;
+			float r = MathUtility.Sqrt(radius2 - (dy * dy));
+
+			Vector3 directionToCenter = Vector3.Normalize(cameraToCenter);
+			Vector3 newCenter = cameraPosition + directionToCenter * (distanceToCenter - dy);
 
 			// Disc aligned with camera
-			Disc.DrawWireframe(primitiveDrawer, cameraPosition, cameraView, cameraProjection, center,
-				Vector3.Normalize(center - cameraPosition), radius * 1.1f, Color.Blue, false);
+			Disc.DrawWireframe(primitiveDrawer, cameraPosition, cameraView, cameraProjection,
+				newCenter, directionToCenter, r, Color.White, false);
 		}
 	}
 }
